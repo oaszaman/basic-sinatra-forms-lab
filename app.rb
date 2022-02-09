c13318bcb3ed500ce03fdaffd077dece0f80c0d0
Shahzab A Zaman <o.aszaman00@gmail.com>
2022-02-09 12:58:33 -0600

    DONE:)

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+        
+        erb :newteam
+    end
+
+    post '/team' do 
+        erb :team
+        @name = params["name"]
+        @coach = params["coach"]
+        @pg = params["pg"]
+        @pf = params["pf"]
+        @sg = params["sg"]
+        @sf = params["sf"]
+        @c = params["c"]
+
+
+        erb :team 
+    end
 
 end
